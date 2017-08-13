aa7f7290dcb8c41ef0e10ebdbbd49fc87b3be7d2
Rishi Karri <rishikarri1@gmail.com>
2017-08-12 21:56:16 -0400

    successfully got styled components working

--- src/App.js
+++ src/App.js
@@ -2,20 +2,49 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import TopNavBar from '../src/Components/TopNavBar/'
+import styled, { keyframes } from 'styled-components';
+
+const rotatingAnimation = keyframes`
+ 
+    0%   {background-color: red;}
+    25%  {background-color: yellow;}
+    50%  {background-color: blue;}
+    100% {background-color: green;}
+`;
+
+const RotatingLetter = styled.div`
+  display: inline-block;
+  color: ${(props) => props.color ? `solid 9px ${props.color}` : 'solid 9px red'}; 
+  height: 60px;
+  width: 60px;  
+  animation: ${(props) => props.blinkRate ? `${rotatingAnimation} ${props.blinkRate} linear infinite` : `${rotatingAnimation} 1.5s linear infinite`};
+`;
+
+// animation: ${(props) => props.blinkRate ? `${pulseAnimation} ${props.blinkRate} linear infinite` : `${pulseAnimation} 1.5s linear infinite`};
+
+
+
+
+
+//App Component
 
 const styles = {
   'hrImaginationWrapper' : {
     color: 'green',
     fontSize: 32, 
     textAlign: 'center'
-  }
+  },
 }
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <div style={styles.hrImaginationWrapper} className='col-xs-12'> HR Imagination </div>
+        <div style={styles.hrImaginationWrapper} className='col-xs-12'> 
+          <div style={styles.hrImaginationText} className='col-xs-12'> HR Imagination </div>
+          <RotatingLetter> H </RotatingLetter>
+
+        </div>
       </div>
     );
   }
